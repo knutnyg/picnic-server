c4c7e3af9f40a8d3cbaca79075b860300753e605
Knut Nygaard <knutnyg@gmail.com>
2016-11-12 15:20:33 +0000

    add files

---
+++ Sources/main.swift
@@ -0,0 +1,18 @@
+import Kitura
+
+// Create a new router
+let router = Router()
+
+// Handle HTTP GET requests to /
+router.get("/api") {
+    request, response, next in
+    response.send("Hello, World!")
+    next()
+}
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8090, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
+
